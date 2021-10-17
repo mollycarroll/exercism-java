7d328dce8f45c702bc8326b76486378f99d3a722
mollycarroll <mollyajcarroll@gmail.com>
2021-10-17 11:20:40 -0600

    implement and test the 2 methods

--- cars-assemble/src/main/java/CarsAssemble.java
+++ cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,18 @@
 public class CarsAssemble {
 
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the AssemblyLine.productionRateperHour() method");
+        if (speed <= 4) {
+            return speed * 221;
+        } else if (speed < 9) {
+            return (speed * 221) * .9;
+        } else if (speed == 9) {
+            return (speed * 221) * .8;
+        } else {
+            return (speed * 221) * .77;
+        }
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the AssemblyLine.workingItemsPerMinute() method");
+        return ((int) Math.round(productionRatePerHour(speed)) / 60);
     }
 }
